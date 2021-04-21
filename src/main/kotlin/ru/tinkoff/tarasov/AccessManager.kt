09d637e9a5104934e8a30383a7ddf65fd192df66
Jorres Tarasov <jorres.tarasov@gmail.com>
2021-04-21 19:16:30 +0300

    IMPROVE: introduce proper resource handling

--- src/main/kotlin/ru/tinkoff/tarasov/AccessManager.kt
+++ src/main/kotlin/ru/tinkoff/tarasov/AccessManager.kt
@@ -3,6 +3,44 @@ package ru.tinkoff.tarasov
 import java.sql.ResultSet
 
 class AccessManager(val connManager: ConnectionManager) {
+
+    fun selectEntryById(id: Int, tableName: String) {
+        val query = "SELECT * FROM $tableName WHERE id = $id;"
+        wrapWithResourseManagement(query, "name")
+    }
+
+    fun idGreaterThanTwo(tableName: String) {
+        val query = "SELECT * FROM $tableName WHERE id > 2"
+        wrapWithResourseManagement(query, "name")
+    }
+
+    fun selectWithJoin(
+        srcTable: String, middleTable: String,
+        srcField: String, middleField: String,
+        targetTable: String, targetField: String
+    ) {
+        connManager.getConnection().use {
+            val query = """
+                SELECT * FROM $targetTable LEFT JOIN (
+                    SELECT * 
+                    FROM $srcTable LEFT JOIN $middleTable 
+                    ON $srcTable.$srcField == $middleTable.$middleField
+                ) as A ON $targetTable.id == A.$targetField;
+            """.trimIndent()
+            wrapWithResourseManagement(query, "name")
+        }
+    }
+
+    fun selectGroupBy(tableName: String, columnName: String) {
+        val query = "SELECT $columnName, COUNT(*) as cnt FROM $tableName GROUP BY $columnName;"
+        wrapWithResourseManagement(query, "name", "cnt")
+    }
+
+    fun selectSortBy(tableName: String, columnName: String) {
+        val query = "SELECT * FROM $tableName ORDER BY $columnName;"
+        wrapWithResourseManagement(query, "name")
+    }
+
     private fun printFields(r: ResultSet, vararg fields: String) {
         println("Request result: ")
         for (field in fields) {
@@ -19,76 +57,13 @@ class AccessManager(val connManager: ConnectionManager) {
         println()
     }
 
-    fun selectEntryById(id: Int, tableName: String) {
-        connManager.getConnection().use {
-            printFields(
-                it.createStatement().executeQuery(
-                    """
-                SELECT * FROM $tableName WHERE id = $id;            
-            """.trimIndent()
-                ),
-                "name"
-            )
-        }
-    }
-
-    fun idGreaterThanTwo(tableName: String) {
-        connManager.getConnection().use {
-            printFields(
-                it.createStatement().executeQuery(
-                    """
-                SELECT * FROM $tableName WHERE id > 2;            
-            """.trimIndent()
-                ),
-                "name"
-            )
-        }
-    }
-
-    fun selectWithJoin(
-        srcTable: String, middleTable: String,
-        srcField: String, middleField: String,
-        targetTable: String, targetField: String
-    ) {
-        connManager.getConnection().use {
-            printFields(
-                it.createStatement().executeQuery(
-                    """
-            SELECT * FROM $targetTable LEFT JOIN (
-                SELECT * 
-                FROM $srcTable LEFT JOIN $middleTable 
-                ON $srcTable.$srcField == $middleTable.$middleField
-            ) as A ON $targetTable.id == A.$targetField;
-        """.trimIndent()
-                ),
-                "name"
-            )
-        }
-    }
-
-    fun selectGroupBy(tableName: String, columnName: String) {
-        connManager.getConnection().use {
-            printFields(
-                it.createStatement().executeQuery(
-                    """
-                SELECT $columnName, COUNT(*) as cnt FROM $tableName GROUP BY $columnName;            
-            """.trimIndent()
-                ),
-                "name", "cnt"
-            )
-        }
-    }
-
-    fun selectSortBy(tableName: String, columnName: String) {
-        connManager.getConnection().use {
-            printFields(
-                it.createStatement().executeQuery(
-                    """
-                SELECT * FROM $tableName ORDER BY $columnName;            
-            """.trimIndent()
-                ),
-                "name"
-            )
+    private fun wrapWithResourseManagement(query: String, vararg fields: String) {
+        connManager.getConnection().use { conn ->
+            conn.createStatement().use { statement ->
+                statement.executeQuery(query).use { resultSet ->
+                    printFields(resultSet, *fields)
+                }
+            }
         }
     }
 }
