dccde3b29d1c754c507eb4fb2fc30b89c018d440
Jorres Tarasov <jorres.tarasov@gmail.com>
2021-04-28 15:28:38 +0300

    NEW: docker-compose setup + sql init scripts

---
+++ init_mysql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE optional (
+    id TEXT NOT NULL,
+    data TEXT NOT NULL
+);
+
+INSERT INTO optional (id, data) 
+    VALUES (1, 'first-optional-piece');
+INSERT INTO optional (id, data) 
+    VALUES (2, 'second-optional-piece');
---
+++ init_postgres.sql
@@ -0,0 +1,9 @@
+CREATE TABLE necessary (
+    id TEXT NOT NULL,
+    data TEXT NOT NULL
+);
+
+INSERT INTO necessary (id, data) 
+    VALUES (1, 'first-piece');
+INSERT INTO necessary (id, data) 
+    VALUES (2, 'second-piece');
